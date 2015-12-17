926664cf6e6b736a81b165ae51890cdcc0d9ed9c
Whitesunset <chester.work@gmail.com>
2015-12-17 23:17:11 +0100

    Feature: random array index

--- array.js
+++ array.js
@@ -12,7 +12,17 @@ ForteArray.prototype.item = function (values, recursive) {
     recursive = recursive || false;
     var result = values[Math.floor(Math.random() * values.length)];
     if(result instanceof Array && recursive){
-        result = this.item(result);
+        result = this.item(result, recursive);
+    }
+    return result;
+}
+
+ForteArray.prototype.index = function (values, recursive) {
+    recursive = recursive || false;
+    var index = Math.floor(Math.random() * values.length),
+        result = [];
+    if(values[index] instanceof Array && recursive){
+        result.push(this.index(values[index], recursive));
     }
     return result;
 }
